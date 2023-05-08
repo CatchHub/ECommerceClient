50cd9ec7f0480d3325d1c5546a3b747f5dd8cd8c
Umut Yilmaz <tumuylmz@gmail.com>
2023-05-08 04:23:17 +0300

    Alterify lib added and customized. This service is just for admin panel. There is a sample in dashboad.component.ts. After this commit i will delete this one.

--- src/app/admin/components/dashboard/dashboard.component.ts
+++ src/app/admin/components/dashboard/dashboard.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AlertifyService, MesageType, Position } from 'src/app/services/admin/alertify.service';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
+  constructor(private alertify: AlertifyService){}
 
+  ngOnInit(): void {
+  
+  }
+
+
+  // this is a sample usage. After this commit i'll delete these codes with html buttons in dashboard.
+  m(){
+    this.alertify.message("Hello World!",
+      {
+        mesageType: MesageType.Warning,
+        position: Position.BottomLeft, 
+        delay : 5, 
+        dismissOthers : true
+      });
+  }
+
+
+  d(){
+    this.alertify.dismissAll();
+  }
 }
